9326da24b5e708993f9c162fc2a04387a57b3b50
Matthew De Leeuw <deleeuwdevelopmentstudio@gmail.com>
2026-01-29 19:17:05 -0800

    initial commit

---
+++ src/lib/PhotoFrame.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    export let src: string;
+    export let alt: string = "Our Memory Together";
+    export let caption: string = "";
+    export let rotation: 'left' | 'right' | 'none' = 'none';
+    const rotateClass = {
+        left: '-rotate-2 hover:rotate-0',
+        right: 'rotate-2 hover:rotate-0',
+        none: 'hover:scale-105'
+    }[rotation];
+</script>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+import ValentineWrapper from '$lib/ValentineWrapper.svelte';
+</script>
+
+<ValentineWrapper>
+    <h1>Will you be my Valentine?</h1>
+
+    <div class="photo-gallery">
+    </div>
+</ValentineWrapper>
